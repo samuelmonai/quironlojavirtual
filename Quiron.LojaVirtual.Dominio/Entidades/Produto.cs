53f2cbadbb6e868db0c3c4b1a1d51768dbac60d3
samuelmonai <samuelmonai@hotmail.com>
2014-12-29 22:52:02 -0200

    Classe Produto

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int ProdutoID { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Descricao { get; set; }
+
+        public Decimal Preco { get; set; }
+
+        public String Categoria { get; set; }
+    }
+}
