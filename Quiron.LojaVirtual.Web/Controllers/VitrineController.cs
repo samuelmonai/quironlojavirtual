4a772f35962e333b531ae457989e661d52112f3e
samuelmonai <samuelmonai@hotmail.com>
2015-01-18 10:32:12 -0200

    Bootstrap

--- Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -4,24 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.Web.Controllers
 {
     public class VitrineController : Controller
     {
         private ProdutosRepositorio _repositorio;
-        public int ProdutoPorPagina = 3;
+        public int ProdutoPorPagina = 8;
         
         public ActionResult ListaProdutos(int pagina = 1)
         {
             _repositorio = new ProdutosRepositorio();
-            var produtos = _repositorio.Produtos
+
+            ProdutosViewModel model = new ProdutosViewModel() 
+            {
+                Produtos = _repositorio.Produtos
                 .OrderBy(p => p.Descricao)
                 .Skip((pagina - 1) * ProdutoPorPagina)
-                .Take(ProdutoPorPagina);
+                .Take(ProdutoPorPagina),
 
-
-            return View(produtos);
+                Paginacao = new Paginacao 
+                {
+                    PaginaAtual = pagina,
+                    ItensPorPagina = ProdutoPorPagina,
+                    ItensTotal = _repositorio.Produtos.Count()
+                }
+            };
+            return View(model);
         }
 	}
 }
